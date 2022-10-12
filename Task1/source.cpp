8ec0f92d88f4a2d0312110b4f89663a088e326c7
Vladislav-Erofeev <92443812+Vladislav-Erofeev@users.noreply.github.com>
2022-10-12 18:52:59 +0300

    Commit

---
+++ Task1/source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <exception>
+#include <string.h>
+#include <windows.h>
+
+void add(int* &arr, int& length, int value)
+{
+    int* temp_array = new int[length + 1];
+    for (int i = 0; i < length; i++)
+    {
+        temp_array[i] = arr[i];
+    }
+    temp_array[length] = value;
+    delete[] arr;
+    arr = temp_array;
+    length++;
+}
+
+bool isInteger(char* str)
+{
+    try {
+        float temp_float = std::stof(str);
+        int temp_integer = std::stoi(str);
+        return (temp_float - temp_integer == 0) ? (true) : (false);
+    }
+    catch (std::invalid_argument e)
+    {
+        return false;
+    }
+}
+
+int separate(std::string input, int*& arr)
+{
+    char delimiters[] = " ";
+    arr = new int[0];
+    int length = 0;
+    char* str = _strdup(input.c_str());
+    char* temp = strtok_s(str, delimiters, &str);;
+    do
+    {
+        if (isInteger(temp))
+        {
+            add(arr, length, std::stoi(temp));
+        }
+        temp = strtok_s(str, delimiters, &str);
+    } while (temp != NULL);
+    return length;
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::string input;
+    int* arr = nullptr;
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, input);
+    int length = separate(input, arr);
+    std::cout << "Получившийся массив: ";
+    for (int i = 0; i < length; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    // Hello
+}
